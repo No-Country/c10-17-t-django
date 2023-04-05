9f388f8513c1968588049ea0312da2ccc9e7a64d
gregjose25131 <gregjose2513@gmail.com>
2023-04-04 23:08:57 -0400

    Creacio y union de modelos

--- tourify_backend/core/aplications/tour_guide/models.py
+++ tourify_backend/core/aplications/tour_guide/models.py
@@ -1,16 +1,16 @@
 from django.db import models
-from authentication.models import CustomUser
+from aplications.authentication.models import CustomUser
 
 STATE_GUIDE = (('1','ACTIVO'),('2','INACTIVO'),('3','BLOQUEADO'))
 
 # Create your models here.
 class Guide(models.Model):
-    dni = models.IntegerField(max_length=150,null=False,blank=False,unique=True,primary_key=True)
+    dni = models.CharField(max_length=150,null=False,blank=False,unique=True,primary_key=True)
     phone = models.CharField(max_length=50,blank=False,null=False)
     address = models.CharField(max_length=50,blank=False,null=False)
     country = models.CharField(max_length=20,blank=False,null=False)
     postal_code = models.CharField(max_length=50,blank=False,null=False)
-    state = models.CharField(choices=STATE_GUIDE,blank=False,null=False)
+    state = models.CharField(max_length=10,choices=STATE_GUIDE,blank=False,null=False)
     id_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
 
 
@@ -19,4 +19,10 @@ class Certification(models.Model):
     institute = models.CharField(max_length=50,null=False,blank=False)
     broadcast_date = models.DateField(null=False,blank=False)
     link = models.CharField(max_length=150,null=False,blank=False)
+    dni = models.ForeignKey(Guide, on_delete=models.CASCADE)
+
+class Review_Guide(models.Model):
+    calification = models.FloatField(null=False,blank=False)
+    comment = models.TextField(null=False,blank=False)
+    id_user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
     dni = models.ForeignKey(Guide, on_delete=models.CASCADE)
