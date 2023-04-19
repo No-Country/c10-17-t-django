ab8b7b8e012515bcbc1f8bbdad3b45531557c5a0
Brian Valeriano <briant.newman09@gmail.com>
2023-04-19 18:38:24 -0300

    Vistas para busqueda segun filtro

--- tourify_backend/core/aplications/tourist_spot/urls.py
+++ tourify_backend/core/aplications/tourist_spot/urls.py
@@ -2,13 +2,17 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('create-site/', CreateSite.as_view(), name='Create Site'),
-    path('add-photo-site/', AddPhotoSite.as_view(), name='Add Photo Site'),
-    path('get-site/', ShowSite.as_view(), name='Get Site'),
-    path('list-photo-site/', ShowListPhoto.as_view(), name='Photos Site'),
-    path('update-site/<str:id_site>/', UpdateSite.as_view(), name='Update Site'),
-    path('list-site', ShowListSite.as_view(), name='List Site'),
-    path('list-guide-site', ShowGuideSite.as_view(), name='List Guide Site'),
-    path('list-favorite-site', ShowFavoriteSite.as_view(), name= 'List Favorite Site'),
-    path('list-visit-site', ShowVisitSite.as_view(), name= 'List Visit site')
+    path('create-site/', CreateSite.as_view(), name='create_site'),
+    path('add-photo-site/', AddPhotoSite.as_view(), name='add_photo_site'),
+    path('get-site/', ShowSite.as_view(), name='get_site'),
+    path('list-photo-site/', ShowListPhoto.as_view(), name='photos_site'),
+    path('update-site/<str:id_site>/', UpdateSite.as_view(), name='update_site'),
+    path('list-site/', ShowListSite.as_view(), name='list_site'),
+    path('list-guide-site/', ShowGuideSite.as_view(), name='list_guide_site'),
+    path('list-favorite-site/', ShowFavoriteSite.as_view(), name= 'list_favorite_site'),
+    path('list-visit-site/', ShowVisitSite.as_view(), name= 'list_visit_site'),
+    path('bookmark/<str:id_user>/<str:id_site>/', BookmarkFavoriteSite.as_view(), name='bookmark_favorite_site'),
+    path('search-name/',SearchSiteByName.as_view(), name='search_site_by_name'),
+    path('search-budget/',SearchSiteByBudget.as_view(), name='search_site_by_budget'),
+    path('search-province/',SearchSiteByProvince.as_view(), name='search_site_by_province')
 ]
--- tourify_backend/core/aplications/tourist_spot/views.py
+++ tourify_backend/core/aplications/tourist_spot/views.py
@@ -78,13 +78,47 @@ class UpdateSite(UpdateAPIView):
 #LISTAR LUGARES TURISTICOS
 class ShowListSite(APIView):
     def get(self, request):
-        validation= self.request.query_params.get('validation')
-        valit_site= Site.objects.filter(validation=validation)
+
+        valit_site= Site.objects.filter(validation=True)
         list_site= SiteSerializer(valit_site, many=True)
 
         if len(list_site.data)!=0:
             return Response(list_site.data, status=status.HTTP_200_OK)
         return Response({"message":"No se encontraron lugares para mostrar"},status=status.HTTP_204_NO_CONTENT)
+    
+#BUSCAR LUGARES SEGUN EL NOMBRE
+class SearchSiteByName(APIView):
+    def get(self,request):
+        search_query=self.request.query_params.get('q')
+        if search_query:
+            valid_site=Site.objects.filter(validation=True, name__icontains=search_query)
+        list_site=SiteSerializer(valid_site, many=True)
+        if len(list_site.data)!=0:
+            return Response(list_site.data, status=status.HTTP_200_OK)
+        return Response({"message":"No se encontraron coincidencias en el nombre"},status=status.HTTP_204_NO_CONTENT)
+        
+#BUSCAR LUGARES SEGUN EL PRESUPUESTO
+class SearchSiteByBudget(APIView):
+    def get(self,request):
+        min_budget = request.query_params.get('min_budget')
+        max_budget = request.query_params.get('max_budget')
+
+        valid_site=Site.objects.filter(validation=True, budget__gte=min_budget, budget__lte=max_budget)
+        list_site=SiteSerializer(valid_site, many=True)
+        if len(list_site.data)!=0:
+            return Response(list_site.data, status=status.HTTP_200_OK)
+        return Response({"message":"No se encontraron lugares segun el presupuesto ingresado"},status=status.HTTP_204_NO_CONTENT)
+
+#BUSCAR LUGARES SEGUN LA PROVINCIA
+class SearchSiteByProvince(APIView):
+    def get(self,request):
+        search_query= self.request.query_params.get('q')
+        if search_query:
+            valid_site=Site.objects.filter(validation=True, province__icontains=search_query)
+        list_site=SiteSerializer(valid_site, many=True)
+        if len(list_site.data)!=0:
+            return Response(list_site.data, status=status.HTTP_200_OK)
+        return Response({"message":"No se encontraron lugares cercanos a tu ubicacion"},status=status.HTTP_204_NO_CONTENT)
 
 #LISTAR GUIAS DE UN LUGAR TURISTICO
 class ShowGuideSite(APIView):
@@ -95,10 +129,10 @@ class ShowGuideSite(APIView):
 
         if len(list_guide.data)!=0:
             return Response(list_guide.data, status=status.HTTP_200_OK)
-        return Response({"message":"No se encontraron lugares para mostrar"},status=status.HTTP_204_NO_CONTENT)
+        return Response({"message":"No se encontraron guias para el lugar seleccionado"},status=status.HTTP_204_NO_CONTENT)
     
 #LISTAR LUGARES FAVORITOS
-class ShowFavoriteSite(APIView):
+"""class ShowFavoriteSite(APIView):
     def get(self,request):
         id_user = self.request.query_params.get('id_user')
         favorite_site= Favorite_site.objects.filter(id_user=id_user)
@@ -106,15 +140,57 @@ class ShowFavoriteSite(APIView):
 
         if len(list_favorite.data)!=0:
             return Response(list_favorite.data, status=status.HTTP_200_OK)
-        return Response({"message":"No se encontaron lugares marcados como favoritos"})
-
-#LISTAR LUGARES VISITADOS
-class ShowVisitSite(APIView):
+        return Response({"message":"No se encontaron lugares marcados como favoritos"})"""
+    
+class ShowFavoriteSite(APIView):
     def get(self,request):
-        id_user= self.request.query_params.get('id_user')
-        visit_site= Visit_site.objects.filter(id_user=id_user)
+        id_user = self.request.query_params.get('id_user')
+        favorite_site= Favorite_site.objects.filter(id_user=id_user)
+        sites = [site.id_site for site in favorite_site]
+        list_site = SiteSerializer(sites, many=True)
+        if len(list_site.data) !=0:
+            return Response(list_site.data, status=status.HTTP_200_OK)
+        return Response({"message":"No se encontraron lugares marcados como favoritos"})
+
+
+#MOSTRAR RESEÑAS DE UN LUGAR TURISTICO
+"""class ShowReviewSite(APIView):
+    def get(self,request):
+        id_site= self.request.query_params.get('id_side')
+        visit_site= Visit_site.objects.filter(id_side=id_side)
         list_visit= VisitSiteSerializer(visit_site, many=True)
 
         if len(list_visit.data)!=0:
             return Response(list_visit.data, status=status.HTTP_200_OK)
-        return Response({"message":"No se encontraron lugares visitados"})
+        return Response({"message":"No se encontraron lugares visitados"})"""
+
+
+#MOSTRAR LUGARES VISITADOS
+class ShowVisitSite(APIView):
+    def get(self,request):
+        id_user= self.request.query_params.get('id_user')
+        visit_site= Visit_site.objects.filter(id_user=id_user)
+        sites=[site.id_site for site in visit_site]
+        list_site = SiteSerializer(sites, many=True)
+        if len(list_site.data) !=0:
+            return Response(list_site.data, status=status.HTTP_200_OK)
+        return Response({"message":"No se encontraron lugares que hallas visitado"})
+    
+#MOSTRAR FOTOS DE UNA VISITA TURISTICO
+class ShowPhotoVisit(APIView):
+    def get(self,request):
+        visit_number= self.request.query_params.get('visit_number')
+        photo_site = Photo_visit.objects.filter(visit_number=visit_number)
+        list_photo = PhotoVisitSerializer(photo_site, many=True)
+
+        if len(list_photo.data)!=0:
+            return Response(list_photo.data, status=status.HTTP_200_OK)
+        return Response({"message":"No se encontraron imagenes para mostrar"},status=status.HTTP_204_NO_CONTENT)
+
+#DENTIFICAR UN LUGAR FAVORITO
+class BookmarkFavoriteSite(APIView):
+    def get(self,request,id_user, id_site):
+        favorite_site = Favorite_site.objects.filter(id_user=id_user,id_site=id_site)
+        if favorite_site.exists():
+            return Response(True)
+        return Response(False)
